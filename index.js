08bc6e6e872bfaba6c4ad36e51e6656dcf8255e6
ilia <ilia>
2022-07-15 20:07:30 +0300

    make it better

--- index.js
+++ index.js
@@ -33,9 +33,12 @@ app.use("/api/orders", orderRouter);
 
 app.use(errorMiddleware);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.resolve(__dirname, "./client/build")));
-}
+
+
+app.use(express.static(path.resolve(__dirname, './client/build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.listen(process.env.PORT || 8800, () => {
 	connectDatabase();
