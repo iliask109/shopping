aff68191d07d3689c3fe0949ab75c895ec5cbcb2
ilia <ilia>
2022-07-15 20:15:15 +0300

    make it better

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./utils/errorMiddleware");
 const path = require("path");
 const app = express();
+const dirname = require("path");
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -33,13 +34,12 @@ app.use("/api/orders", orderRouter);
 
 app.use(errorMiddleware);
 
+app.use(express.static(path.resolve(__dirname,"/client/build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname,"/client/build", "index.html"));
+});
 
-
-app.use(express.static(path.resolve(__dirname, './client/build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-})
-
+console.log(__dirname,"/client/build");
 app.listen(process.env.PORT || 8800, () => {
 	connectDatabase();
 	console.log("content to backend");
