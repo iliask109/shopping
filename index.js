cdbdc5ec65c987c2806e26f12418502abe8f238f
ilia <ilia>
2022-07-16 08:41:54 +0300

    make it better

--- index.js
+++ index.js
@@ -34,12 +34,12 @@ app.use("/api/", productRouter);
 app.use("/api/orders", orderRouter);
 
 app.use(errorMiddleware);
-// app.use(express.static(path.resolve(__dirname, "client/build")));
+app.use(express.static(path.resolve(__dirname, "client/build")));
 
-// app.get("*", (req, res) => {
-// 	const index = path.join(__dirname, "client/build", "index.html");
-// 	req.sendFile(index);
-// });
+app.get("*", (req, res) => {
+	const index = path.join(__dirname, "client/build", "index.html");
+	req.sendFile(index);
+});
 
 app.listen(process.env.PORT || 8800, () => {
 	connectDatabase();
